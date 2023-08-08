e69774b4e1eb4a4955f8d9c69ea8e442946dc690
Kaarthik <Kaarthik.n@techfinite.com>
2023-08-08 18:41:50 +0530

    company name change

--- logistics/logistics/doctype/billing_job/billing_job.py
+++ logistics/logistics/doctype/billing_job/billing_job.py
@@ -238,7 +238,7 @@ class BillingJob(Document):
             "shipping_address": "No 4, Sengunthapuram",
             "billing_address": "No 4, Sengunthapuram",
             "items": items,
-            "set_warehouse": "Stores - DL"
+            "set_warehouse": "Stores - DLPL"
             # "selling_price_list": "Standard S"
         })
         return sales_order
