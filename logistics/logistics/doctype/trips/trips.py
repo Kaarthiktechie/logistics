e98fb9d158c1947dca2d009c1ae540498c2e901d
Kaarthik <Kaarthik.n@techfinite.com>
2023-08-24 18:17:28 +0530

    removed multiple nonwanted items

--- logistics/logistics/doctype/trips/trips.py
+++ logistics/logistics/doctype/trips/trips.py
@@ -5,36 +5,21 @@ import frappe
 from frappe.model.document import Document
 from datetime import date
 
-class Trips(Document):
-	def init(self):
-		# self.today = date.today()
-		self.trips =[]
-		self.trips.clear()	
+class Trips(Document):	
   
 	def validate(self):
-		self.init()
-		self.function()
-			
-	def function(self):
-		# truck_no = self.gettruck_no()
 		self.table_insert()
-
-	def gettruck_no(self):
-		asset_id = frappe.db.get_list("Asset", filters={"name": self.asset_name}, fields =["item_code"])[0]
-		truck_no = asset_id.item_code
-		return truck_no
-
+  
 	def table_insert(self):
 		tripsstatus = frappe.get_doc({
 			"doctype": "Trip details",
-			"asset_name": self.asset_name,
-			"status" : self.status,
 			"driver": self.driver,
+			"asset_name": self.asset_name,
+			"trip" : self.trip,
 			"status" : self.status,
 			"date"	: self.date,
-			"trip" : self.trip,
-			# "item_code" : truck_no
-   
+			"time" : self.time,
+			"km" : self.km
 		})
 		if tripsstatus:
 			tripsstatus.insert()
