c965add2a2729777b30308ef40a155b1df9594a5
Kaarthik <Kaarthik.n@techfinite.com>
2023-08-30 11:54:24 +0530

    empty commit

--- logistics/logistics/doctype/driver_login_page/driver_login_page.js
+++ logistics/logistics/doctype/driver_login_page/driver_login_page.js
@@ -1,8 +1,19 @@
 // Copyright (c) 2023, techfinite and contributors
 // For license information, please see license.txt
-
 frappe.ui.form.on('Driver Login Page', {
-	// refresh: function(frm) {
-
-	// }
+    refresh: function(frm) {
+        frm.fields_dict['reports_in'].$input.on('click', function() {
+            // Redirect to a new URL
+            frappe.call({
+                method:"logistics.logistics.doctype.driver_login_page.driver_login_page.function"
+            })
+            window.location.href = "http://localhost:8000/app/todo/25b0133c6a";
+        });
+    }
 });
+
+
+
+
+
+
